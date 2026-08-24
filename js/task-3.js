6ae7ff6787f84086817778edd6c21916340d198f
dasha0707 <rigkovadasha@gmail.com>
2026-08-24 14:21:43 +0300

    Fix task 3 according to feedback

--- js/task-3.js
+++ js/task-3.js
@@ -3,6 +3,6 @@ function getElementWidth(content, padding, border) {
   return totalWidth;
 }
 
-console.log(getElementWidth(50, 8, 4)); // 74
-console.log(getElementWidth(60, 12, 8.5)); // 101
-console.log(getElementWidth(200, 0, 0)); // 200 // 200
+console.log(getElementWidth(50, 8, 4));
+console.log(getElementWidth(60, 12, 8.5));
+console.log(getElementWidth(200, 0, 0));
